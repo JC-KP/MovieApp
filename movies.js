21467f68d45fe7c8288b3abfef0717c811db5100
Jake Chadwell <jake.chadwell01@gmail.com>
2021-04-20 13:41:17 -0500

    added html cards

--- movies.js
+++ movies.js
@@ -1,25 +1,31 @@
 //Initial Fetch request
 let url = 'https://endurable-mercurial-dosa.glitch.me/movies'
-
-let fetchResponse= fetch(url)
-    .then(response => (console.log(response.json())))
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
-
-//Loader for the page
-const loader = document.querySelector(".loader");
-window.onload = function(){
-    setTimeout(function(){
-        loader.style.opacity = "0";
-        setTimeout(function(){
-            loader.style.display = "none";
-        }, 500);
-    },1500);
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
 }
-//Posting the API info to HTML
-$.get(url, {
-    limit:  10,
-    offset: 20
-}).done(function(data) {
-    // do something with the data
-});
+let fetchResponse= fetch(url, options)
+        .then(response => response.json())
+
+console.log(fetchResponse);
+
+
+// Loader for the page
+// const loader = document.querySelector(".loader");
+// window.onload = function(){
+//     setTimeout(function(){
+//         loader.style.opacity = "0";
+//         setTimeout(function(){
+//             loader.style.display = "none";
+//         }, 500);
+//     },1500);
+// }
+// //Posting the API info to HTML
+// $.get(url, {
+//     limit:  10,
+//     offset: 20
+// }).done(function(data) {
+//     // do something with the data
+// });
