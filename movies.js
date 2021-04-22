17a8ebe20ed2a43d8d16c9f01bdf21f298e56571
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-04-22 11:24:32 -0500

    Completed POST functionallity for user to add new movie via Navbar

--- movies.js
+++ movies.js
@@ -1,3 +1,9 @@
+// Wait for window load
+// $(window).on("load", function () {
+//     // Animate loader off screen
+//     $(".se-pre-con").fadeOut("slow");
+// });
+
 // //Initial Fetch request
 let url = 'https://endurable-mercurial-dosa.glitch.me/movies'
 const options = {
@@ -28,69 +34,37 @@ for (let i= 0; i< results.length; i++) {
             '<li class="list-group-item">' + 'Director: ' + results[i].director + '</li>' +
             '<li class="list-group-item">' + 'Year: ' + results[i].year + '</li>' +
             '<li class="list-group-item">' + 'Rating: ' + results[i].rating + '</li>' +
-            '<li class="list-group-item">' + 'Plot: ' + results[i].plot + '</li>' +
             '<li class="list-group-item">' + 'Cast: ' + results[i].actors + '</li>' +
             '</ul>' +
             '</div>'
         $('.card-deck').append(html)
     }
 }}
-
-
-
-
-$('#submit').click(function (){
-    console.log('hey')
+    // SUBMITTING USER MOVIE INTO INPUTS
+$('#submit').click(function (e){
+    e.preventDefault()
     let userMovie = {
         title: $('#movie-title').val(),
-        rating: $('#movie-rating').val()
+        director: $('#movie-director').val(),
+        year: $('#movie-year').val(),
+        rating: $('#movie-rating').val(),
+        actors: $('#movie-cast').val()
     };
 
-    function renderUserMovies(){
 
-        if (userMovie.title !== undefined && userMovie.rating !== undefined) {
-            let html = ""
-            html = '<div class="card ">' +
-                '<ul class=" list-group list-group-flush">' +
-                '<li class="list-group-item">' + 'Title: ' + userMovie.title + '</li>' +
-                '<li class="list-group-item">' + 'Rating: ' + userMovie.rating + '</li>' +
-                '</ul>' +
-                '</div>'
-            $('.card-deck').append(html)
-        }
-    }
-
-    let post = {
-        method: 'POST',
+//Post user movie
+    fetch(url, {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(userMovie)
-    }
-    fetch(url, post)
-        .then(results=>{
-            renderUserMovies(results)
+    })
+        .then((response) => { return response.json() })
+        .then(function (results) {
+            console.log(results);
+            renderMovies(results)
+            location.reload()
         })
-
-
 })
-//
-//
-//
-// // Loader for the page
-// // const loader = document.querySelector(".loader");
-// // window.onload = function(){
-// //     setTimeout(function(){
-// //         loader.style.opacity = "0";
-// //         setTimeout(function(){
-// //             loader.style.display = "none";
-// //         }, 500);
-// //     },1500);
-// // }
-// // //Posting the API info to HTML
-// // $.get(url, {
-// //     limit:  10,
-// //     offset: 20
-// // }).done(function(data) {
-// //     // do something with the data
-// // });
+
